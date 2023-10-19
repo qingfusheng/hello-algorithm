2deb29c79b9ec365a3d63a3f9e9c1ce3d78f7c60
青青子衿 <61900087+qingfusheng@users.noreply.github.com>
2023-10-19 15:28:05 +0800

    Update 27.ClimbingStairs_MinCost.cpp

--- code/27.ClimbingStairs_MinCost.cpp
+++ code/27.ClimbingStairs_MinCost.cpp
@@ -1,32 +1,34 @@
+// 爬楼梯(最小代价)
+// return: 最小代价（而不是所有方案数量）
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 /* 爬楼梯最小代价：动态规划 */
+
+// 使用dp数组
 int minCostClimbingStairsDP(vector<int> &cost) {
+    // cost : [0, 1, 2] (其中还包括了第0个台阶，指当前状态，还没跨出的时候cost为0)
     int n = cost.size() - 1;
-    if (n == 1 || n == 2)
+    if(n == 1 || n == 2)
         return cost[n];
-    // 初始化 dp 表，用于存储子问题的解
-    vector<int> dp(n + 1);
-    // 初始状态：预设最小子问题的解
+    vector<int> dp(n+1);
     dp[1] = cost[1];
     dp[2] = cost[2];
-    // 状态转移：从较小子问题逐步求解较大子问题
-    for (int i = 3; i <= n; i++) {
-        dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
-    }
+    for(int i=3;i<=n;i++)
+        dp[i] = min(dp[i-1], dp[i-2]) + cost[i];
     return dp[n];
 }
-/* 爬楼梯最小代价：空间优化后的动态规划 */
-int minCostClimbingStairsDPComp(vector<int> &cost) {
+// 使用变量替代数组，减小了空间占用
+int minCostClimbingStairsDPcomp(vector<int>& cost){
     int n = cost.size() - 1;
-    if (n == 1 || n == 2)
+    if(n==1 || n==2)
         return cost[n];
-    int a = cost[1], b = cost[2];
-    for (int i = 3; i <= n; i++) {
-        int tmp = b;
-        b = min(a, tmp) + cost[i];
-        a = tmp;
+    int a=cost[1], b = cost[2];
+    for(int i=3;i<=n;i++){
+        int temp = b;
+        b = min(a,b)+cost[i];
+        a = temp;
     }
     return b;
 }
@@ -34,6 +36,7 @@ int minCostClimbingStairsDPComp(vector<int> &cost) {
 
 int main(){
     vector<int> costs{0, 1,10,1};
-    int result = minCostClimbingStairsDP(costs);
+    int reuslt1 = minCostClimbingStairsDP(costs);
+    int result = minCostClimbingStairsDPcomp(costs);
     cout<<"result:"<<result;
 }
