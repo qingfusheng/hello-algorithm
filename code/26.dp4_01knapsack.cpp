1e0f8c1a2c47c4921643ce643c12142598aa4d69
青青子衿 <61900087+qingfusheng@users.noreply.github.com>
2023-10-23 23:43:36 +0800

    20231023
    占个commit坑位

--- code/26.dp4_01knapsack.cpp
+++ code/26.dp4_01knapsack.cpp
@@ -7,11 +7,13 @@ int knapsackDP(vector<int>& wgt, vector<int>& val, int cap){
     int n = wgt.size();
     vector<vector<int>> dp(n+1, vector<int>(cap+1, 0));
     for(int i=1;i<=n;i++){
-        for(int c=1; c<=cap;c++){
-            if(wgt[i-1] > c)
+        for(int c=1;c<=cap;c++){
+            if(wgt[i-1] > c){
+                // 这是背包里面已经装不下i-1了
                 dp[i][c] = dp[i-1][c];
-            else
+            }else{
                 dp[i][c] = max(dp[i-1][c], dp[i-1][c-wgt[i-1]]+val[i-1]);
+            }
         }
     }
     return dp[n][cap];
@@ -19,26 +21,33 @@ int knapsackDP(vector<int>& wgt, vector<int>& val, int cap){
 /* 0-1 背包：空间优化后的动态规划 */
 int knapsackDPComp(vector<int> &wgt, vector<int> &val, int cap) {
     int n = wgt.size();
-    // 初始化 dp 表
+    // 为什么这里从正序到逆序就可以将dp从二维缩减到一维
     vector<int> dp(cap + 1, 0);
-    // 状态转移
-    for (int i = 1; i <= n; i++) {
-        // 倒序遍历
-        for (int c = cap; c >= 1; c--) {
-            if (wgt[i - 1] <= c) {
-                // 不选和选物品 i 这两种方案的较大值
-                dp[c] = max(dp[c], dp[c - wgt[i - 1]] + val[i - 1]);
+    for(int i=1; i<=n; i++){
+        for(int c=cap;c>=1;c--){
+            cout<<"i:"<<i<<",c:"<<c<<endl;
+            if(wgt[i-1] > c){
+                cout<<"背包容量小了装不下了:"<<wgt[i-1]<<endl;
+                dp[c] = dp[c];
+            }else{
+                cout<<"选择不装的价值:"<<dp[c]<<",VS选择装的价值:"<<dp[c-wgt[i-1]]+val[i-1];
+                dp[c] = max(dp[c], dp[c-wgt[i-1]]+val[i-1]);
+                cout<<",结果价值为："<<dp[c]<<endl;
             }
         }
+        for(int c=cap;c>=0;c--)
+            cout<<c<<":"<<dp[c]<<" ,";
+        cout<<endl;
     }
     return dp[cap];
+    // dp[50] = max([wgt:10]+dp[40], [wgt:20]+dp[30])
 }
 
 int main(){
     int Cap, N;
-    Cap = 50, N=5;
-    vector<int> wgt{10,20,30,40,50};
-    vector<int> val{50, 120, 150, 210, 240};
+    Cap = 5, N=5;
+    vector<int> wgt{1,2,3,4,5};
+    vector<int> val{5, 12, 15, 21, 24};
     // vector<vector<int>> wgt_val{{10,50},{20, 120},{30, 150},{40, 210},{50, 240}};
     int result1 = knapsackDP(wgt, val, Cap);
     int result2 = knapsackDPComp(wgt, val, Cap);
